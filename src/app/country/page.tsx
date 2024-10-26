4c98e8d322c9573c586d1376d53c3b90ed781760
YUMNANASIR01 <yumnanasir719@gmail.com>
2024-10-26 09:54:33 -0700

    second

--- src/app/country/page.tsx
+++ src/app/country/page.tsx
@@ -5,7 +5,7 @@ function Country() {
   return (
     <>
     <ul>
-          <hr /><li><h1 className="bg-slate-500 font-extrabold mb-7 items-center mr-11">Click Below To Visit Country 👇</h1></li><hr />
+          <hr /><li><h1 className="bg-slate-500 font-extrabold mb-6 items-center mr-11 visit">Click Below To Visit Country 👇</h1></li><hr />
         
          <hr /> <li className="bg-lime-600"><hr />  <Link className=" font-bold  " href={"/country/canada"}><h2>🔹Canada</h2></Link></li><hr />
          <hr />  <li className="bg-lime-600"><hr /> <Link className=" font-bold " href={"/country/germany"}><h2>🔹Germany</h2></Link></li><hr />
